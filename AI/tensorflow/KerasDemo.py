e7d6fba5f632be96ac6fbbad90fd77b7b7951f0c
zhaopeng <peng.zhao@togic.com>
2020-06-03 16:12:31 +0800

    tensorflow backup

---
+++ AI/tensorflow/KerasDemo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- encoding:utf-8 -*-
+
+"""
+@license:   (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@software:  PyCharm
+@file:      PythonTools_KerasDemo.py
+@author:    ziv
+@time:      2020/6/3
+@version:   v1.0.0
+@desc:      脚本功能描述
+"""
+
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
